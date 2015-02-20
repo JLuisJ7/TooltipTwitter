09455f1cadb201e3a4a452247d0a0c7127fc0c14
jluisj7 <jluisjab7@gmail.com>
2015-02-20 16:16:24 -0500

    codigo y archivo js para las funcionalidades del plugin

--- tooltipTwitter.php
+++ tooltipTwitter.php
@@ -9,3 +9,37 @@ Author URI:
 License:GLP2 
 */
 ?>
+
+<?php 
+add_action('wp_head','tooltiptwitter');
+
+function tooltiptwitter()
+{
+
+ ?>
+    <script>var twitterAccount = "jluisjab";</script>
+    <div class="shareTooltip" id="shareTooltip">
+        	<div class="tooltipContainer"><a id="sendToTwitter" href="" class="sharingLink twitter"><span></span></a></div>
+     </div>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+    
+
+ <?php } ?>
+
+<?php
+function wpb_adding_styles() {
+wp_register_style('my_stylesheet', plugins_url('css/style.css', __FILE__));
+wp_enqueue_style('my_stylesheet');
+}
+
+add_action( 'wp_enqueue_scripts', 'wpb_adding_styles' );  
+?>
+ <?php 
+/* Registro de nuestro script. */
+ function wpb_adding_scripts() {
+wp_register_script('my_app_script', plugins_url('js/app.js', __FILE__));
+wp_enqueue_script('my_app_script');
+}
+
+add_action( 'wp_enqueue_scripts', 'wpb_adding_scripts' );  
+?>
