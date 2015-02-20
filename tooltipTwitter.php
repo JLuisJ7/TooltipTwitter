725ff94a7b567daf6cd8c059db737d7ec76c2ef5
jluisab7 <jlj_7@hotmail.com>
2015-02-20 16:10:07 -0500

    Plantilla del plugin creado

---
+++ tooltipTwitter.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name:TootipTweet
+Plugin URI: https://github.com/JLuisJ7/TooltipTwitter
+Description: Este es un plugin que sirve para compartir en twitter cualquier texto que selecciones dentro de la pagina.
+Version: 1.0
+Author: J. Luis J.
+Author URI:
+License:GLP2 
+*/
+?>
